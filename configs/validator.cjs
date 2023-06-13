e540ad7319169fa5692e3bf2f8f6e7dc61e620f1
Loris Leiva <loris.leiva@gmail.com>
2023-06-14 00:33:30 +0100

    Use new program scripts

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -1,8 +1,9 @@
 const path = require("path");
 
 const programDir = path.join(__dirname, "..", "programs");
-function getProgram(dir, programName) {
-  return path.join(programDir, dir, "target", "deploy", programName);
+
+function getProgram(programBinary) {
+  return path.join(programDir, ".bin", programBinary);
 }
 
 module.exports = {
@@ -12,7 +13,20 @@ module.exports = {
       {
         label: "Mpl Project Name",
         programId: "MyProgram1111111111111111111111111111111111",
-        deployPath: getProgram("mpl-project-name", "mpl_project_name.so"),
+        deployPath: getProgram("mpl_project_name.so"),
+      },
+      // Below are external programs that should be included in the local validator.
+      // You may configure which ones to fetch from the cluster when building
+      // programs within the `configs/program-scripts/dump.sh` script.
+      {
+        label: "Token Metadata",
+        programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+        deployPath: getProgram("mpl_token_metadata.so"),
+      },
+      {
+        label: "SPL Noop",
+        programId: "noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV",
+        deployPath: getProgram("spl_noop.so"),
       },
     ],
   },
